64f7b2f8d7aa9d6f323d4e1842f234f2433102e4
WesGoodman <56207706+WesGoodman@users.noreply.github.com>
2019-10-10 11:17:42 -0700

    Add files via upload
    initial

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "images/dice" + randomNumber1 + ".png"
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png"
+
+var image1 = document.querySelectorAll("img")[0];
+
+var image2= document.querySelectorAll('img')[1];
+
+image1.setAttribute('src', randomDiceImage);
+image2.setAttribute('src', randomDiceImage2);
+
+var title = document.querySelector("h1");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').innerHTML= "Player One Wins!"
+    
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector('h1').innerHTML= "Player two Wins!"
+    
+} else {
+    document.querySelector('h1').innerHTML = "Draw"
+};
